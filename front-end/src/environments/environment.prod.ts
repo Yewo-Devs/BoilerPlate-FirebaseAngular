8134ed2ae4f52e692753355e07cbc4b78b66bef0
Yewo-Devs <85104248+Yewo-Devs@users.noreply.github.com>
2025-01-12 19:59:57 +0200

    Update clientUrl in environment files
    The `clientUrl` property in both `environment.prod.ts` and `environment.ts` files has been updated. Previously, the `clientUrl` was set to `'https://localhost:7294/'`, and it has now been changed to `'https://localhost:4200/'`. This change affects both the production and development environments.

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://localhost:7294/',
-  clientUrl: 'https://localhost:7294/',
+  clientUrl: 'https://localhost:4200/',
   googleClientId: 'YOUR_GOOGLE_CLIENT_ID',
   saasName: 'SaaS Name',
   saasUrl: 'https://www.my-saas.com/',
